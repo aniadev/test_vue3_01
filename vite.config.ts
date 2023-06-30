d8df8872c50210f6352020b11c10c2926c13b6a5
phamhai99 <haipc@beedu.vn>
2023-06-30 18:57:18 +0700

    fix build tools

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 const markdownRawPlugin = require('vite-raw-plugin')
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,8 @@ export default defineConfig({
     vueJsx(),
     markdownRawPlugin({
       fileRegex: /\.md$/
-    })
+    }),
+    chunkSplitPlugin()
   ],
   resolve: {
     alias: {
